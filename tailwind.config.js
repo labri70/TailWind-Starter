ab149affc0c26204bd6788fbe3f987bf9a6d71d0
labri70 <labri70@naver.com>
2023-09-12 11:47:45 +0900

    TailWind Starter

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  prefix: '',
+  important: false,
+  separator: ':',
+
+  theme: {},
+  variants: {},
+  corePlugins: {},
+  plugins: [],
+}
